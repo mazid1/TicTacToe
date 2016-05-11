7d475720fef63048aade4efa3b5d29ddc101f17f
mazid1 <mazidmailbox@gmail.com>
2016-05-12 00:05:10 +0600

    Few more activities added

--- app/src/main/java/com/chholamuri/tictactoe/gameModeActivity.java
+++ app/src/main/java/com/chholamuri/tictactoe/gameModeActivity.java
@@ -7,18 +7,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class gameModeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_game_mode);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_game_mode, menu);
         return true;
     }
 
@@ -37,13 +37,8 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void exitApp(View view) {
-        finish();
-        System.exit(0);
-    }
-
-    public void playGame(View view) {
-        Intent intent = new Intent(this, gameModeActivity.class);
+    public void startOnePlayerMatch(View view) {
+        Intent intent = new Intent(this, DifficultyActivity.class);
         startActivity(intent);
     }
 }
